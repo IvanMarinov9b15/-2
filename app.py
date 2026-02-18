b0955cfb6d416e18123cc85f93500d9113fffbab
IvanMarinov9b15 <ipmarinov248b@gmail.com>
2026-02-18 14:17:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+if "books" not in st.session_state:
+  st.session_state.books = []
+title = st.text_input("Title")
+author = st.text_input("Author")
+
+if st.button("Add book"):
+  book = {
+    "title": title,
+    "author": author
+  }
+  st.sesion_state.books.append(book)
+  st.success("The book is added!")
+
+st.write("### List with books:")
+st.write(st.session_state.books)
